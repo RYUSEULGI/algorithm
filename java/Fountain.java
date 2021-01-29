02c6b651d84b80e94667e1664959ddf5507b1174
RYUWEULGI <rhythm296@naver.com>
2021-01-29 15:38:28 +0900

    1193번(실패)

---
+++ java/Fountain.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Fountain {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int cnt = 0;
+        int line = 0;
+
+        while (cnt < a) {
+            line++;
+            // cnt를 정의 해주어야 함
+        }
+
+        // 짝수일 경우 홀수일 경우 나눠서 생각하기
+        if (line % 2 == 0) {
+
+        } else {
+
+        }
+    }
+}
+
+// 규칙
+// 1번 1/1
+// 2번 1/2 2/1
+// 3번 1/3 2/2 3/1
+// 4번 1/4 2/3 3/2 4/1
+// 5번 1/5 2/4 3/3 4/2
