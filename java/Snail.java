665412b6c5b0a4371c74fe15d3933638b4fbaa15
RYUWEULGI <rhythm296@naver.com>
2021-02-02 11:58:28 +0900

    2869번(실패)

---
+++ java/Snail.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("올라갈 수 있는 길이");
+        int up = scan.nextInt();
+
+        System.out.println("미끄러지는 길이");
+        int down = scan.nextInt();
+
+        System.out.println("총 막대 길이");
+        int total = scan.nextInt();
+
+        int day = (total - down) / (up - down);
+
+        if (day != 0) {
+            day++;
+        }
+        System.out.println(day + "일이 걸린다");
+    }
+}
+
+// day 1 2 3 4
+// num 2 1 3 2 4 3 5
+// climb +2 -1 +2 -1 +2 -1 +2
